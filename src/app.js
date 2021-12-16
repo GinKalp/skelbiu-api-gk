ac3ecfc7c4b69be1ee98381e847412a089fa9a1c
GinKalp <80583818+GinKalp@users.noreply.github.com>
2021-12-16 08:37:54 +0200

    mini fixes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 const morgan = require('morgan');
 const cors = require('cors')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 
 const authRouter = require('./routes/authRouter')
 const listingsRouter = require('./routes/listingsRouter')
